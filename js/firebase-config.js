5441f55b0e2a9767e4f415a9691d67a165fe36fe
Caio Giacomelli <caio.h.giacomelli@gmail.com>
2026-08-31 12:15:39 -0300

    Added firebase rules

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,21 @@
-// ===== FIREBASE CONFIG - CURSE OF STRAHD =====
-// IMPORTANTE: Crie um novo projeto Firebase para esta campanha
-// 1. Vá em https://console.firebase.google.com
-// 2. Crie um novo projeto (ex: "barovia-wiki")
-// 3. Ative o Firestore Database
-// 4. Nas configurações do projeto, copie os dados abaixo
-// 5. Configure as regras do Firestore (permitir leitura/escrita)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "COLE_SUA_API_KEY_AQUI",
-    authDomain: "SEU-PROJETO.firebaseapp.com",
-    projectId: "SEU-PROJETO",
-    storageBucket: "SEU-PROJETO.firebasestorage.app",
-    messagingSenderId: "000000000000",
-    appId: "1:000000000000:web:0000000000000000"
+  apiKey: "AIzaSyDsbyAHIVb9ccZY9ZwT2Zc93wEHI92A1vU",
+  authDomain: "barovia-wiki.firebaseapp.com",
+  projectId: "barovia-wiki",
+  storageBucket: "barovia-wiki.firebasestorage.app",
+  messagingSenderId: "319336145382",
+  appId: "1:319336145382:web:f9c21513535135e9ee3627",
+  measurementId: "G-9DS94W54MN"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
